0d83480a8fd6b0e258f26205154e9cd5bce86668
Leo Prieto <leop22@outlook.fr>
2022-06-14 17:09:20 +0200

    Crash tested models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :lists
+
+  validates :email, :nickname, :user_location, presence: true
+  validates :email, format: { with: /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/ }, uniqueness: true
 end
