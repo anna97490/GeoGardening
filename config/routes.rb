c86a9160ef3753f328918f8e70acbd775d9ae1c8
Leo Prieto <leop22@outlook.fr>
2022-06-15 10:59:19 +0200

    Routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  
+  resources :plants, only: %i[index show] do
+    resources :plant_lists, only: :create
+  end
+
+  resources :lists, only: %i[index new create show destroy]
+
+  resources :plant_lists, only: :destroy
 end
