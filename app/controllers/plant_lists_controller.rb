bfac5df3b9250bb6628bff404a872ed67a58898f
Leo Prieto <leop22@outlook.fr>
2022-06-15 13:41:31 +0200

    added actions in plant_list controller

--- app/controllers/plant_lists_controller.rb
+++ app/controllers/plant_lists_controller.rb
@@ -1,7 +1,12 @@
 class PlantListsController < ApplicationController
   def create
+    @plant_list = PlantList.new(list_id: params[:list_id], plant_id: params[:plant_id]) if params[:plant_id].present?
+    redirect_to plants_path, notice: "Congratulations! You have added a plant to the list. " if @plant_list.save
   end
 
   def destroy
+    @plant_list = PlantList.find(params[:id])
+    @plant_list.destroy
+    redirect_to plant_lists_path
   end
 end
