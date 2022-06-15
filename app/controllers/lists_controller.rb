f6794bbc4f962ad4ca26145c3f69506ebedf3b08
Leo Prieto <leop22@outlook.fr>
2022-06-15 12:33:18 +0200

    Add actions in lists_controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,16 +1,34 @@
 class ListsController < ApplicationController
   def index
+    @lists = List.all
   end
 
   def new
+    @list = List.new(params[:id])
   end
 
   def create
+    @list = List.new(list_params)
+    @list.location = @current_user.user_location if params[:location].blank?
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def destroy
+    @list.destroy
+    redirect_to lists_url, notice: 'List was successfully deleted'
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, :location)
   end
 end
