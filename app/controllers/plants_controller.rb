8e8a89477e3ecfde1c59dc0b77b767ea87f4df17
Leo Prieto <leop22@outlook.fr>
2022-06-16 17:33:25 +0200

    Search created

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -2,10 +2,25 @@ class PlantsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
-    @plants = Plant.all
+    @all_plants = Plant.new
+    if params_form[:name].blank? && params_form[:plant_type].blank?
+      @plants = Plant.all
+    elsif params_form[:name].blank?
+      @plants = Plant.where("plant_type LIKE ?", "%#{params_form[:plant_type]}%")
+    elsif params_form[:plant_type].blank?
+      @plants = Plant.where("name LIKE ?", "%#{params_form[:name]}%")
+    else
+      @plants = Plant.where("name LIKE ? AND plant_type LIKE ?", "%#{params_form[:name]}%", "%#{params_form[:plant_type]}%")
+    end
   end
 
   def show
     @plant = Plant.find(params[:id])
+  end
+
+  private
+
+  def params_form
+    params.require(:plant).permit(:name, :plant_type)
   end
 end
