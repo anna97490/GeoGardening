c6f1e7f8f8a768131f588ace92771a8ac2fbe567
Ekaterina Dzeraeva <katja.dzeraeva@gmail.com>
2022-06-17 08:34:09 +0200

    created a class method for creating plants

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,4 +1,8 @@
 class Plant < ApplicationRecord
   has_many :lists, through: :plant_lists
   has_many :plant_lists
+  def self_create_from_collection(plants)
+    plants.each do |plant_hash|
+      Plant.create(plant_hash)
+  end
 end
