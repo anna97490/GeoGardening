15a9a12191590aac089f2f8e58649c827547299a
Leo Prieto <leop22@outlook.fr>
2022-06-20 16:21:40 +0200

    Pleins de trucs

--- app/testing/test.rb
+++ app/testing/test.rb
@@ -6,7 +6,7 @@ def transform(string)
     array = []
     chars.each do |c|
       if /^[0-9]+$/ === c
-      array << c
+        array << c
       end
     end
     if array.include? array[2]
@@ -16,18 +16,17 @@ def transform(string)
       new_array << a
       range = Range.new(new_array[0], new_array[1]).to_a
       results = range.map do |c|
-      c.to_i
-        end
+        c.to_i
+      end
       results
-      else
-        range = Range.new(array[0], array[1]).to_a
-        results = range.map do |c|
-          c.to_i
-          end
-        results
+    else
+      range = Range.new(array[0], array[1]).to_a
+      results = range.map do |c|
+        c.to_i
+      end
+      results
     end
   end
-
-
+end
 
 # transform("6a-9")
