cc3fd81db9249971b5d18357a0b7ca1696fecb6f
stephanie joseph <jos.stephanie@gmail.com>
2022-06-22 18:22:19 +0200

    meta-yml fix

--- config/initializers/default_meta.rb
+++ config/initializers/default_meta.rb
@@ -1,2 +1,9 @@
 # Initialize default meta tags.
-DEFAULT_META = YAML.load_file(Rails.root.join("config/meta.yml"))
+DEFAULT_META = {
+meta_product_name: "GeoGardening",
+meta_title: "GeoGardening - Mettez-vous au vert en fonction de votre zone géographique",
+meta_description: "Découvrez quelles plantes sont adaptées à votre zone climatique."
+}
+
+# Initialize default meta tags.
+# DEFAULT_META = YAML.load_file(Rails.root.join("../config/meta.yml"))
